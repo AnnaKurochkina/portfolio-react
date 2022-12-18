ee9730f72c2bfd3f123709cb7c1e0d0bbb2c23e0
Anna Kurochkina <basia220586@gmail.com>
2022-12-18 21:47:15 +0000

    Work in progress

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,8 +4,9 @@ import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
     return (
-        <footer id="contact" className="contacts"> 
-        <a href="mailto: basia220586@gmail.com">basia220586@gmail.com</a>
+        <footer id="contact" className="contacts">
+			<h1>My contacts:</h1><br/>
+		<a href="mailto: basia220586@gmail.com">basia220586@gmail.com</a>
         <a href="https://www.linkedin.com/in/anna-kurochkina-99244321b/" target="blank"><FaLinkedin/></a>
         <a href="https://github.com/AnnaKurochkina" target="blank"><FaGithub/></a>
         </footer>
