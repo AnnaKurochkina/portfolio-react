71480cb9c809798904d497dc20f2e133e507c199
Anna Kurochkina <basia220586@gmail.com>
2023-02-10 14:29:09 +0000

    NavBar media query tweak in mobile view

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,6 @@ import Banner from "./Components/Banner/Banner";
 import Projects from "./Components/Projects/Projects";
 import Skills from "./Components/Skills/Skills";
 import About from "./Components/About/About";
-import { faHome } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Footer from "./Components/Footer/Footer";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
